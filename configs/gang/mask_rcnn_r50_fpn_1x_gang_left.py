2c27680e59c11aae7c19acacc03e6ae6735d1b93
jiangwenj02 <jiangwenj02@gmail.com>
2021-01-11 09:42:12 +0800

    adding gang

---
+++ configs/gang/mask_rcnn_r50_fpn_1x_gang_left.py
@@ -0,0 +1,81 @@
+_base_ = [
+    '../_base_/models/mask_rcnn_r50_fpn.py',
+    '../_base_/datasets/coco_instance.py',
+    '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
+]
+
+model = dict(
+    pretrained=None,
+    roi_head=dict(
+        bbox_head=dict(
+            type='Shared2FCBBoxHead',
+            in_channels=256,
+            fc_out_channels=1024,
+            roi_feat_size=7,
+            num_classes=8,
+            bbox_coder=dict(
+                type='DeltaXYWHBBoxCoder',
+                target_means=[0., 0., 0., 0.],
+                target_stds=[0.1, 0.1, 0.2, 0.2]),
+            reg_class_agnostic=False,
+            loss_cls=dict(
+                type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0),
+            loss_bbox=dict(type='SmoothL1Loss', beta=1.0, loss_weight=1.0)),
+        mask_head=dict(
+            type='FCNMaskHead',
+            num_convs=4,
+            in_channels=256,
+            conv_out_channels=256,
+            num_classes=8,
+            loss_mask=dict(
+                type='CrossEntropyLoss', use_mask=True, loss_weight=1.0))))
+
+dataset_type = 'GangDataset'
+data_root = '/data2/qilei_chen/DATA/ShanghaiAutograding/gangganpingheng_images_240'
+
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
+    dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels', 'gt_masks']),
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(1333, 800),
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=32),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+data = dict(
+    samples_per_gpu=2,
+    workers_per_gpu=2,
+    train=dict(
+        type=dataset_type,
+        ann_file='/home/zzhang/left.json',
+        img_prefix='/data2/qilei_chen/DATA/ShanghaiAutograding/gangganpingheng_images_240',
+        pipeline=train_pipeline),
+    val=dict(
+        type=dataset_type,
+        ann_file='/home/zzhang/left.json',
+        img_prefix='/data2/qilei_chen/DATA/ShanghaiAutograding/gangganpingheng_images_240',
+        pipeline=test_pipeline),
+    test=dict(
+        type=dataset_type,
+        ann_file='/home/zzhang/left.json',
+        img_prefix='/data2/qilei_chen/DATA/ShanghaiAutograding/gangganpingheng_images_240',
+        pipeline=test_pipeline))
+evaluation = dict(metric=['bbox', 'segm'])
