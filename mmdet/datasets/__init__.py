185affb0abfc65211714e10ad7b39f8412440b23
jiangwenj02 <jiangwenj02@gmail.com>
2021-01-12 22:01:43 +0800

    adding polyp

--- mmdet/datasets/__init__.py
+++ mmdet/datasets/__init__.py
@@ -12,11 +12,12 @@ from .voc import VOCDataset
 from .wider_face import WIDERFaceDataset
 from .xml_style import XMLDataset
 from .gangganpingheng import GangDataset
+from .polyp import PolypDataset
 __all__ = [
     'CustomDataset', 'XMLDataset', 'CocoDataset', 'DeepFashionDataset',
     'VOCDataset', 'CityscapesDataset', 'LVISDataset', 'LVISV05Dataset',
     'LVISV1Dataset', 'GroupSampler', 'DistributedGroupSampler',
     'DistributedSampler', 'build_dataloader', 'ConcatDataset', 'RepeatDataset',
     'ClassBalancedDataset', 'WIDERFaceDataset', 'DATASETS', 'PIPELINES',
-    'build_dataset', 'replace_ImageToTensor', 'GangDataset'
+    'build_dataset', 'replace_ImageToTensor', 'GangDataset', 'PolypDataset'
 ]
