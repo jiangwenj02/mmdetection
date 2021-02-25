e86a929bd3fe855a232a491a4e9c23efd3974039
jiangwenj02 <jiangwenj02@gmail.com>
2021-02-25 20:25:10 +0800

    fix some bug

--- configs/adenomatous/mask_rcnn_r50_fpn_1x_adenomatous.py
+++ configs/adenomatous/mask_rcnn_r50_fpn_1x_adenomatous.py
@@ -106,12 +106,10 @@ data = dict(
     samples_per_gpu=2,
     workers_per_gpu=2,
     train=dict(
-        type='RepeatDataset',
-        times=10,
-        dataset=dict(type=dataset_type,
+        type=dataset_type,
         ann_file='/data0/zzhang/adenomatous/train.json',
         img_prefix='/data2/dataset/cleaned_data',
-        pipeline=train_pipeline)),
+        pipeline=train_pipeline),
     val=dict(
         type=dataset_type,
         ann_file='/data0/zzhang/adenomatous/test.json',
