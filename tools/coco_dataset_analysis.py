e200cc3482bb3557f0666d5ee9c150d51364f5d5
jiangwenj02 <jiangwenj02@gmail.com>
2021-03-08 10:32:00 +0800

    fix bugs

--- tools/coco_dataset_analysis.py
+++ tools/coco_dataset_analysis.py
@@ -179,7 +179,7 @@ def main():
     parser = ArgumentParser(description='COCO Dataset Analysis Tool')
     parser.add_argument(
         '--ann',
-        default='data/adenomatous/annotation/train.json',
+        default='/data0/annotation/adenomatous/train.json',
         help='annotation file path')
     parser.add_argument(
         '--types',
