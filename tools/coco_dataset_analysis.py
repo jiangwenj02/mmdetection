f143d5fae88bb3c8f556bedc8a521cd34311c084
jiangwenj02 <jiangwenj02@gmail.com>
2021-03-08 10:33:42 +0800

    fix paths

--- tools/coco_dataset_analysis.py
+++ tools/coco_dataset_analysis.py
@@ -179,7 +179,7 @@ def main():
     parser = ArgumentParser(description='COCO Dataset Analysis Tool')
     parser.add_argument(
         '--ann',
-        default='/data0/annotation/adenomatous/train.json',
+        default='/data0/zzhang/annotation/adenomatous/train.json',
         help='annotation file path')
     parser.add_argument(
         '--types',
