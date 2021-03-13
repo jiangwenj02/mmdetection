b2960461c0a13eded34abab5b79267592aceafaf
jiangwenj02 <jiangwenj02@gmail.com>
2021-03-13 09:31:13 +0800

    fix some bugs

--- tools/merge_json.py
+++ tools/merge_json.py
@@ -2,20 +2,20 @@ import json
 import glob
 
 # ANNOT_PATH = '/home/zxy/Datasets/VOC/annotations/'
-ANNOT_PATH = 'voc/annotations/'
-OUT_PATH = ANNOT_PATH
+#ANNOT_PATH = 'voc/annotations/'
+OUT_PATH = './data/erosive/annotations/'
 # INPUT_FILES = ['pascal_train2012.json', 'pascal_val2012.json',
 #                'pascal_train2007.json', 'pascal_val2007.json']
-dirs = './data/erosive/annotations/train/'
-INPUT_FILES = glob.glob('*.json')
-OUTPUT_FILE = 'train.json'
-KEYS = ['images', 'type', 'annotations', 'categories']
+dirs = './data/erosive/annotations/test/'
+INPUT_FILES = glob.glob(dirs+'*.json')
+OUTPUT_FILE = 'test.json'
+KEYS = ['images', 'annotations', 'categories']
 MERGE_KEYS = ['images', 'annotations']
 
 out = {}
 tot_anns = 0
 for i, file_name in enumerate(INPUT_FILES):
-  data = json.load(open(ANNOT_PATH + file_name, 'r'))
+  data = json.load(open(file_name, 'r'))
   print('keys', data.keys())
   if i == 0:
     for key in KEYS:
@@ -30,4 +30,5 @@ for i, file_name in enumerate(INPUT_FILES):
     print(file_name, 'annotations', len(data['annotations']))
   tot_anns = len(out['annotations'])
 print('tot', len(out['annotations']))
+print('images', len(out['images']))
 json.dump(out, open(OUT_PATH + OUTPUT_FILE, 'w'))
