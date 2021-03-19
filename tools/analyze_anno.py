c67c28fd04e2a6659b9942934c166cdc53c2b96f
jiangwenj02 <jiangwenj02@gmail.com>
2021-03-19 23:31:28 +0800

    add some

---
+++ tools/analyze_anno.py
@@ -0,0 +1,68 @@
+from pycocotools.coco import COCO
+
+def test_data():
+    # Specify the path to model config and checkpoint file
+    # model_name = 'reppoints_moment_r50_fpn_1x_coco'
+    # score_thr=0.25
+    # config_file = 'configs/erosive/faster_rcnn_r50_fpn_1x_erosive.py'
+    # checkpoint_file = '/data0/zzhang/mmdetection/erosive/work_dirs/'+model_name+'/epoch_83.pth'
+    # build the model from a config file and a checkpoint file
+    #model = init_detector(config_file, checkpoint_file, device='cuda:0')
+    # test images and show the results
+    set_name = 'test' #['train','test']
+    anns_file = './data/erosive/annotations/test.json'
+    #anns_file = '/data1/qilei_chen/DATA/erosive/annotations/'+set_name+'.json'
+    coco_instance = COCO(anns_file)
+    coco_imgs = coco_instance.imgs
+    count_images_with_anns = 0
+    count_images_without_anns = 0
+    count_anns = 0
+    img_file_name_list = {}
+    img_file_name_dict = {}
+    count_zero_ann = 0
+    for key in coco_imgs:
+        annIds = coco_instance.getAnnIds(imgIds= coco_imgs[key]['id'])
+        anns = coco_instance.loadAnns(annIds)
+        if not len(anns)==0:
+            count_images_with_anns+=1
+            count_anns+=len(anns)
+        else:
+            count_images_without_anns+=1
+        img_file_name = coco_imgs[key]["file_name"]
+        if not img_file_name in img_file_name_dict:
+            img_file_name_dict[img_file_name] = 0
+        else:
+            img_file_name_dict[img_file_name] += 1
+            print(img_file_name)
+        if not img_file_name in img_file_name_list:
+            img_file_name_list[img_file_name] = []
+            img_file_name_list[img_file_name].append(anns)
+        else:
+            print(img_file_name)
+            print(img_file_name_list[img_file_name])
+            if len(img_file_name_list[img_file_name][0])==0:
+                count_zero_ann+=1
+            print(anns)
+        '''
+        img_file_name = coco_imgs[key]["file_name"]
+        img_dir = os.path.join("/data1/qilei_chen/DATA/erosive/images",img_file_name)
+        img = mmcv.imread(img_dir)
+        result = inference_detector(model, img)
+        for ann in anns:
+            [x,y,w,h] = ann['bbox']
+            cv2.rectangle(img, (int(x), int(y)), (int(x+w), int(y+h)), (0,255,0), 2)
+        model.show_result(img, result,score_thr=score_thr,bbox_color =(255,0,0),
+                        text_color = (255,0,0),font_size=5,
+                        out_file='/data1/qilei_chen/DATA/erosive/work_dirs/'+model_name+'/'+set_name+'_result_'+str(score_thr)+'/'+img_file_name)
+        '''
+    for key, value in img_file_name_list.items():
+        if len(value[0])==0:
+            count_zero_ann+=1
+    print(len(img_file_name_dict))
+    print(count_images_with_anns)
+    print(count_anns)
+    print(count_images_without_anns)
+    print(len(img_file_name_list))
+    print(count_zero_ann)
+
+test_data()
--- tools/merge_json.py
+++ tools/merge_json.py
@@ -5,12 +5,20 @@ import shutil
 image_id = 1
 annotation_id = 0
 
-adenomatous_json_dir = 'annotations/test'
+adenomatous_json_dir = 'data/erosive/annotations/test'
 inflammatory_json_dir = '/Users/xinzisun/Documents/new_polyp_annotation/Inflammatory/train'
 hyperplastic_json_dir = '/Users/xinzisun/Documents/new_polyp_annotation/Hyperplastic/train'
 image_root = ''
 dataset_root = ''
-out_json = 'test.json'
+out_json = 'data/erosive/annotations/test.json'
+
+# merged_data = {
+#                 "licenses": [{"name": "", "id": 0, "url": ""}],
+#                 "info": {"contributor": "", "date_created": "2021-2", "description": "", "url": "", "version": 1, "year": "2020"},
+#                 "categories": [{"id": 1, "name": "Adenomatous", "supercategory": ""}, {"id": 2, "name": "Inflammatory", "supercategory": ""}, {"id": 3, "name": "Hyperplastic", "supercategory": ""}],
+#                 "images": [],
+#                 "annotations": []
+# }
 
 merged_data = {
                 "licenses": [{"name": "", "id": 0, "url": ""}],
@@ -20,6 +28,13 @@ merged_data = {
                 "annotations": []
 }
 
+# merged_data = {
+#                 "licenses": [{"name": "", "id": 0, "url": ""}],
+#                 "info": {"contributor": "", "date_created": "2021-2", "description": "", "url": "", "version": 1, "year": "2020"},
+#                 "categories": [{"id": 1, "name": "Polyp", "supercategory": ""}, {"id": 2, "name": "Instrument", "supercategory": ""}],
+#                 "images": [],
+#                 "annotations": []
+# }
 
 img_id_test = set()
 imgs_num = 0
@@ -48,7 +63,43 @@ for f in os.listdir(adenomatous_json_dir):
         image_id += len(data["images"])
         annotation_id += len(data["annotations"])
 print('images %d, annos %d'%(imgs_num, anno_num))
+# for f in os.listdir(inflammatory_json_dir):
+#     if f == '.DS_Store':
+#         continue
+#     json_dir = os.path.join(inflammatory_json_dir, f)
+#     with open(json_dir) as json_file:
+#         data = json.load(json_file)
+#         for img in data['images']:
+#             img['id'] += image_id
+#             img['file_name'] = 'colon_inflammatory/' + '/'.join(img['file_name'].split('/')[2:])
+#             merged_data["images"].append(img)
+#         for anno in data["annotations"]:
+#             anno['id'] += annotation_id
+#             anno['category_id'] = 2
+#             anno['image_id'] += image_id
+#             merged_data['annotations'].append(anno)
+            
+#         image_id += len(data['images'])
+#         annotation_id += len(data["annotations"])
 
+# for f in os.listdir(hyperplastic_json_dir):
+#     if f == '.DS_Store':
+#         continue
+#     json_dir = os.path.join(hyperplastic_json_dir, f)
+#     with open(json_dir) as json_file:
+#         data = json.load(json_file)
+#         for img in data["images"]:
+#             img['id'] += image_id
+#             img['file_name'] = 'colon_hyperplastic/' + '/'.join(img['file_name'].split('/')[2:])
+#             merged_data["images"].append(img)
+#         for anno in data["annotations"]:
+#             anno['id'] += annotation_id
+#             anno['category_id'] = 2
+#             anno['image_id'] += image_id
+#             merged_data['annotations'].append(anno)
+
+#         image_id += len(data["images"])
+#         annotation_id += len(data["annotations"])
 
 with open(out_json, 'w') as out_file:
     json.dump(merged_data, out_file)
