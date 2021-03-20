b9e90c42b194760c5fb4f5cb7a020bf29fe144a0
jiangwenj02 <jiangwenj02@gmail.com>
2021-03-20 19:35:50 +0800

    add script

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,4 +4,5 @@ python tools/test.py configs/gang/mask_rcnn_r50_fpn_1x_gang_left.py work_dirs/ma
 python tools/test.py configs/gang/mask_rcnn_r50_fpn_1x_gang_right.py work_dirs/mask_rcnn_r50_fpn_1x_gang_right/latest.pth --eval bbox segm --options "classwise=True" --out work_dirs/mask_rcnn_r50_fpn_1x_gang_right/result.pkl
 python tools/test.py configs/gang/mask_rcnn_r50_fpn_1x_gang_right.py work_dirs/mask_rcnn_r50_fpn_1x_gang_right/latest.pth --format-only --options "jsonfile_prefix=work_dirs/mask_rcnn_r50_fpn_1x_polyp/"
 python tools/eval_others.py --result work_dirs/faster_rcnn_r50_fpn_1x_erosive_9x/result.bbox.json --ann /data0/zzhang/annotation/erosive/test.json --num_clsses 1
+python tools/test.py configs/erosive/faster_rcnn_r50_fpn_1x_erosive_9x.py work_dirs/faster_rcnn_r50_fpn_1x_erosive_9x/latest.pth --format-only --options "jsonfile_prefix=work_dirs/faster_rcnn_r50_fpn_1x_erosive_9x/result"
 #CUDA_VISIBLE_DEVICES=8,9,10,11 ./tools/dist_test.sh configs/polyp/solov2_r50_fpn_polyp_8gpu_3x.py work_dirs/solov2_r50_fpn_8gpu_3x_poly/latest.pth 4 --eval segm --out work_dirs/solov2_r50_fpn_8gpu_3x_poly/result.pkl
