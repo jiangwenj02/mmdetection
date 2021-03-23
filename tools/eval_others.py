4d8a2dd38fb91bbfadb1b2824546b6ed9f0cacf5
jiangwenj02 <jiangwenj02@gmail.com>
2021-03-23 23:42:36 +0800

    fix error

--- tools/eval_others.py
+++ tools/eval_others.py
@@ -19,7 +19,7 @@ def json_load(file_name):
 
 def analyze_results(anno, result, cfg, visualize=False, visualization_folder='./work_dirs/mask_rcnn_r50_fpn_1x_polyp/', testset=None):
     threshold_list = np.arange(0, 1, 0.01).tolist()
-    threshold_list = [0.25]
+    #threshold_list = [0.25]
     coco = COCO(anno)
     img_ids = coco.getImgIds()
     num_cls = cfg.num_clsses
