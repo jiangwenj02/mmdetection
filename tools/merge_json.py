8991298b35bb37e81e532e4f21f03930a0b04a0b
jiangwenj02 <jiangwenj02@gmail.com>
2021-03-26 22:01:46 +0800

    trainall

--- tools/merge_json.py
+++ tools/merge_json.py
@@ -5,18 +5,10 @@ import shutil
 image_id = 0
 annotation_id = 0
 
-adenomatous_json_dir = 'data/ulcer/test'
+adenomatous_json_dir = '/data0/zzhang/annotation/ulcer/'
 image_root = ''
 dataset_root = ''
-out_json = 'data/ulcer/test.json'
-
-# merged_data = {
-#                 "licenses": [{"name": "", "id": 0, "url": ""}],
-#                 "info": {"contributor": "", "date_created": "2021-2", "description": "", "url": "", "version": 1, "year": "2020"},
-#                 "categories": [{"id": 1, "name": "Adenomatous", "supercategory": ""}, {"id": 2, "name": "Inflammatory", "supercategory": ""}, {"id": 3, "name": "Hyperplastic", "supercategory": ""}],
-#                 "images": [],
-#                 "annotations": []
-# }
+out_json = 'data/ulcer/trainall.json'
 
 merged_data = {
                 "licenses": [{"name": "", "id": 0, "url": ""}],
@@ -26,19 +18,11 @@ merged_data = {
                 "annotations": []
 }
 
-# merged_data = {
-#                 "licenses": [{"name": "", "id": 0, "url": ""}],
-#                 "info": {"contributor": "", "date_created": "2021-2", "description": "", "url": "", "version": 1, "year": "2020"},
-#                 "categories": [{"id": 1, "name": "Polyp", "supercategory": ""}, {"id": 2, "name": "Instrument", "supercategory": ""}],
-#                 "images": [],
-#                 "annotations": []
-# }
-
 img_id_test = set()
 imgs_num = 0
 anno_num = 0
 for f in os.listdir(adenomatous_json_dir):
-    if f == '.DS_Store':
+    if f == '.DS_Store' or '.josn' not in f:
         continue
     json_dir = os.path.join(adenomatous_json_dir, f)
     with open(json_dir) as json_file:
@@ -61,43 +45,6 @@ for f in os.listdir(adenomatous_json_dir):
         image_id += len(data["images"])
         annotation_id += len(data["annotations"])
 print('images %d, annos %d'%(imgs_num, anno_num))
-# for f in os.listdir(inflammatory_json_dir):
-#     if f == '.DS_Store':
-#         continue
-#     json_dir = os.path.join(inflammatory_json_dir, f)
-#     with open(json_dir) as json_file:
-#         data = json.load(json_file)
-#         for img in data['images']:
-#             img['id'] += image_id
-#             img['file_name'] = 'colon_inflammatory/' + '/'.join(img['file_name'].split('/')[2:])
-#             merged_data["images"].append(img)
-#         for anno in data["annotations"]:
-#             anno['id'] += annotation_id
-#             anno['category_id'] = 2
-#             anno['image_id'] += image_id
-#             merged_data['annotations'].append(anno)
-            
-#         image_id += len(data['images'])
-#         annotation_id += len(data["annotations"])
-
-# for f in os.listdir(hyperplastic_json_dir):
-#     if f == '.DS_Store':
-#         continue
-#     json_dir = os.path.join(hyperplastic_json_dir, f)
-#     with open(json_dir) as json_file:
-#         data = json.load(json_file)
-#         for img in data["images"]:
-#             img['id'] += image_id
-#             img['file_name'] = 'colon_hyperplastic/' + '/'.join(img['file_name'].split('/')[2:])
-#             merged_data["images"].append(img)
-#         for anno in data["annotations"]:
-#             anno['id'] += annotation_id
-#             anno['category_id'] = 2
-#             anno['image_id'] += image_id
-#             merged_data['annotations'].append(anno)
-
-#         image_id += len(data["images"])
-#         annotation_id += len(data["annotations"])
 
 with open(out_json, 'w') as out_file:
     json.dump(merged_data, out_file)
