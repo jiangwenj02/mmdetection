50cb074c99f23cd27abc9a4d964a5703fc3b9e82
jiangwenj02 <jiangwenj02@gmail.com>
2021-04-03 20:52:30 +0800

    fix

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,4 +6,5 @@ python tools/test.py configs/gang/mask_rcnn_r50_fpn_1x_gang_right.py work_dirs/m
 python tools/eval_others.py --result work_dirs/faster_rcnn_r50_fpn_1x_erosive_9x/result.bbox.json --ann /data0/zzhang/annotation/erosive/test.json --num_clsses 1
 python tools/eval_others.py --result work_dirs/faster_rcnn_r50_fpn_1x_erosive_9x/result.bbox.json --ann /data0/zzhang/annotation/erosive/test.json --num_clsses 1 --config configs/erosive/faster_rcnn_r50_fpn_1x_erosive_9x.py --visualize True --out_dir /data0/zzhang/tmp/erosive_gt_det
 python tools/test.py configs/erosive/faster_rcnn_r50_fpn_1x_erosive_9x.py work_dirs/faster_rcnn_r50_fpn_1x_erosive_9x/latest.pth --format-only --options "jsonfile_prefix=work_dirs/faster_rcnn_r50_fpn_1x_erosive_9x/result"
+python tools/browse_dataset.py configs/erosiveulcer/faster_rcnn_r50_fpn_1x_erosiveulcer.py --output-dir /data0/zzhang/tmp/erosiveulcer/ --not-show
 #CUDA_VISIBLE_DEVICES=8,9,10,11 ./tools/dist_test.sh configs/polyp/solov2_r50_fpn_polyp_8gpu_3x.py work_dirs/solov2_r50_fpn_8gpu_3x_poly/latest.pth 4 --eval segm --out work_dirs/solov2_r50_fpn_8gpu_3x_poly/result.pkl
