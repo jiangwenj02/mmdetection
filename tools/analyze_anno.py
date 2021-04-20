35b3e1888a9517f2b53433273e664cbf3b129a4f
jiangwenj02 <jiangwenj02@gmail.com>
2021-04-20 11:21:17 +0800

    fix

--- tools/analyze_anno.py
+++ tools/analyze_anno.py
@@ -80,8 +80,8 @@ def test_data(anns_file):
     print(count_zero_ann)
 
 
-anns_dir = '/data0/zzhang/annotation/erosive2/train.json'
-anns_dir = 'data/erosive2/annotations/train_removed.json'
+anns_dir = '/data0/zzhang/annotation/erosive2/test.json'
+anns_dir = 'data/erosive2/annotations/test/fine_test.json'
 files = glob.glob(anns_dir)
 for ann_file in files:
     test_data(ann_file)
