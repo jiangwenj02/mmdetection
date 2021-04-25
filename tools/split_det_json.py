724956fd814ba517cfad92df4a0da0aebda2172e
jiangwenj02 <jiangwenj02@gmail.com>
2021-04-25 20:06:42 +0800

    fix

--- tools/split_det_json.py
+++ tools/split_det_json.py
@@ -12,9 +12,9 @@ annotation_id = 0
 
 image_root = ''
 dataset_root = ''
-pre_json = '/data0/zzhang/annotation/ulcer/trainall.json'
-filter_filename = '/data0/zzhang/tmp/ulcer_gt_det/filter_filename.txt'
-outputfile = ['/data0/zzhang/tmp/ulcer_gt_det/filterfiletxt/filter_filename' + str(i) + '.txt' for i in range(12)]
+pre_json = '/data3/zzhang/annotation/ulcer/trainall.json'
+filter_filename = '/data3/zzhang/tmp/ulcer_gt_det/filter_filename.txt'
+outputfile = ['/data3/zzhang/tmp/ulcer_gt_det/filterfiletxt/filter_filename' + str(i) + '.txt' for i in range(12)]
 f_list = [open(file, 'w') for file in outputfile]
 
 filter_file = []
