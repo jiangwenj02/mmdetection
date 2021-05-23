b1dbe6906251d00c0faa2d0bba33207af43ab0e5
jiangwenj02 <jiangwenj02@gmail.com>
2021-05-23 22:22:17 +0800

    debug

--- tools/inference.py
+++ tools/inference.py
@@ -257,6 +257,7 @@ def peval(result_dir, coco_instance, thresh=0.3, with_empty_images=True):
     eval = Metric()
 
     for img_id in results:
+        print(img_id)
         filed_boxes = filt_boxes(results[img_id]['result'],categories, thresh)
         gtannIds = coco_instance.getAnnIds(imgIds=img_id)
         gtanns = coco_instance.loadAnns(gtannIds)
