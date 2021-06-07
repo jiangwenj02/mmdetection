665e7feab0cd448b6bfe0f9bddb94e5641117f6b
MyCurveNet <137376864@qq.com>
2021-06-07 23:10:13 +0800

    debug

--- configs/uav/faster_rcnn_r50_fpn_1x_uav.py
+++ configs/uav/faster_rcnn_r50_fpn_1x_uav.py
@@ -70,3 +70,15 @@ data = dict(
         img_prefix=data_root + 'antiUAVimages',
         pipeline=test_pipeline))
 evaluation = dict(metric=['bbox'])
+
+# optimizer
+optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
+optimizer_config = dict(grad_clip=None)
+# learning policy
+lr_config = dict(
+    policy='step',
+    warmup='linear',
+    warmup_iters=500,
+    warmup_ratio=0.001,
+    step=[6, 7])
+runner = dict(type='EpochBasedRunner', max_epochs=8)
