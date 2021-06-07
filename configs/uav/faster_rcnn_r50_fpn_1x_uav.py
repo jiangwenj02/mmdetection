d7212b76e533eb7a0e948e831448ad153391a836
MyCurveNet <137376864@qq.com>
2021-06-07 23:16:39 +0800

    debug

--- configs/uav/faster_rcnn_r50_fpn_1x_uav.py
+++ configs/uav/faster_rcnn_r50_fpn_1x_uav.py
@@ -52,7 +52,7 @@ test_pipeline = [
         ])
 ]
 data = dict(
-    samples_per_gpu=2,
+    samples_per_gpu=4,
     workers_per_gpu=2,
     train=dict(
         type=dataset_type,
@@ -72,7 +72,7 @@ data = dict(
 evaluation = dict(metric=['bbox'])
 
 # optimizer
-optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
+optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=None)
 # learning policy
 lr_config = dict(
