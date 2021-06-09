ba3afd601d0abd00748b2dbe03be85cc268067cf
MyCurveNet <137376864@qq.com>
2021-06-09 23:00:46 +0800

    fix

--- configs/uav/faster_rcnn_r50_fpn_1x_uav.py
+++ configs/uav/faster_rcnn_r50_fpn_1x_uav.py
@@ -72,7 +72,7 @@ data = dict(
 evaluation = dict(metric=['bbox'])
 
 # optimizer
-optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)
+optimizer = dict(type='SGD', lr=0.03, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=None)
 # learning policy
 lr_config = dict(
