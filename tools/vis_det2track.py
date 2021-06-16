70869922d794af80a558a9416b1d24711dd13a6c
jiangwenj02 <jiangwenj02@gmail.com>
2021-06-16 21:26:07 +0800

    fix

--- tools/vis_det2track.py
+++ tools/vis_det2track.py
@@ -110,12 +110,12 @@ def main(mode='IR', visulization=False):
                             (frame.shape[1] // 2 - 20, 30), 1, 2, (0, 255, 0) if _exist else (0, 0, 255), 2)
                 if len(out) > 0:
                     cv2.putText(frame, '%.3f'%(out[-1]),
-                                (frame.shape[1] // 2 + 40, 30), 1, 2, (0, 255, 255), 2)
+                                (frame.shape[1] // 2 + 100, 30), 1, 2, (0, 255, 255), 2)
                     cv2.rectangle(frame, (int(out[0]), int(out[1])), (int(out[0] + out[2]), int(out[1] + out[3])),
                                 (0, 255, 255))
                 else:
                     cv2.putText(frame, 'not exist',
-                            (frame.shape[1] // 2 + 40, 30), 1, 2, (0, 255, 255), 2)
+                            (frame.shape[1] // 2 + 100, 30), 1, 2, (0, 255, 255), 2)
                 cv2.imshow(video_name, frame)
                 cv2.waitKey(1)
                 frame_list.append(frame)
