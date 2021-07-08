091c483ca6be646ed46e0b2da8f5f9f6d6a829fc
jiangwenj02 <jiangwenj02@gmail.com>
2021-07-08 15:20:40 +0800

    fix

--- tools/dataset_converters/coco_grabcut.py
+++ tools/dataset_converters/coco_grabcut.py
@@ -98,6 +98,7 @@ def grabcut(args):
             for idx, data_ann in enumerate(data['annotations']):
 
                 if data_ann['id'] == ann_id:
+                    data_ann['area'] = img_mask.sum()
                     data_ann['segmentation'] = mask_util.encode(np.array(img_mask[:, :, np.newaxis], order='F', dtype='uint8'))[0]
                     data['annotations'][idx] = data_ann
                     break
